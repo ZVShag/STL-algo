79c9d01a4ed1bd696babe6c14f64dd7f5f90cda3
Alexey <alextutor72@gmail.com>
2022-08-29 18:25:11 +0500

    add find

--- STL algo/STL algo.cpp
+++ STL algo/STL algo.cpp
@@ -30,6 +30,8 @@ public:
 		cout << "Pass\t=\t" << pass << "\tMassa\t=" << massa << 
 			"\tBort_number\t=\t" << number<<endl;
 	}
+
+	
 };
 bool srtairplane(airplane &x, airplane &y)
 {
@@ -40,11 +42,20 @@ int main()
 {   // Возможность выбора сортировки по массе и по номеру самолета
 	airplane a1(2, 200, "IL2");
 	airplane a2(150, 20000, "tu154");
+	airplane a3(20, 100, "IL32");
+	airplane a4(1500, 2000000, "tu1054");
+	airplane a5(350, 30000, "Boing");
+	airplane a6(750, 100000, "Airbus");
 	cout << "for pass sort pres 1" << endl << "for mass sort pres 2"
 		<< endl << "for bnumber sort pres 3" << endl;
 	int k = 0;
 	cin >> k;
-	vector<airplane> a = { a1,a2};
+	vector<airplane> a = { a1,a2,a3,a4,a5,a6 };
+
+	vector<int>b = { 4,5,66,7,545,23,121,7676,7 };
+	
+	
+	
 	if (k == 1) 
 	{
 		sort(a.begin(), a.end(), [](airplane& x, airplane& y)
